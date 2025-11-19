30c93f25e26f936030b78fa6cd4535dd287f27b5
Sonyo <elya.vaisse@gmail.com>
2025-11-19 09:22:34 +0100

    Corrected downloadURL

--- src/header.meta.js
+++ src/header.meta.js
@@ -8,7 +8,7 @@
 // @icon         https://cdn.bsky.app/img/avatar/plain/did:plc:kwmxodxbf5nshavpy5r5l3jj/bafkreiaddzuq5vgrpi3aeufp7gwkbameb426d4vb4zlxvc6c4vo23wkn5a@jpeg
 // @source       https://github.com/Sonyo-UwU/Camoverlay
 // @updateURL    https://github.com/Sonyo-UwU/Camoverlay/raw/refs/heads/main/out/Camoverlay.user.js
-// @downloadURL  https://raw.githubusercontent.com/Sonyo-UwU/Camoverlay/main/dist/Camoverlay.user.js
+// @downloadURL  https://raw.githubusercontent.com/Sonyo-UwU/Camoverlay/main/out/Camoverlay.user.js
 // @match        https://wplace.live/*
 // @run-at       document-body
 // @grant        GM_addStyle
