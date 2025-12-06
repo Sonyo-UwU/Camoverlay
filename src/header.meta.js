63b343d33544b4409afab2968984d5801d87b805
Sonyo <elya.vaisse@gmail.com>
2025-12-06 14:42:02 +0100

    White copy button icon

--- src/header.meta.js
+++ src/header.meta.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         Camoverlay
 // @namespace    https://github.com/Sonyo-UwU/
-// @version      0.5.15
+// @version      0.5.16
 // @description  A remake of Blue Marble
 // @author       Sonyo
 // @license      ISC
